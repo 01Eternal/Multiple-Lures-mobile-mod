da724a18aff2e949a16f4dbbe47a8ec60da37d6a
TLEternal <109238149+01Eternal@users.noreply.github.com>
2024-12-17 22:23:39 -0300

    Added a limit of 55 baits, changed Main Text to Combat New Text, and fixed multiple random baits' positions.

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ import { using } from './ModClasses.js';
 
 using('Terraria');
 using('Terraria.Chat');
+using('Microsoft.Xna.Framework.Graphics')
 
+let NewCombatText =
+    CombatText[
+        "int NewText(Rectangle location, Color color, string text, bool dramatic, bool dot)"
+    ];
+    
 let activeBait = 0;
 let quantityBait = 0;
 
@@ -17,7 +23,7 @@ function loadConfig() {
         try {
             const configData = JSON.parse(tl.file.read("ModConfig.json"));
             quantityBait = configData.quantityBait || 0;
-            tl.log(`[Multiple Lures]: baits ${quantityBait} Loaded.`);
+            tl.log(`[Multiple Lures]: ModConfig Loaded.`);
         } catch (error) {
             quantityBait = 0;
         }
@@ -42,9 +48,18 @@ ChatCommandProcessor.ProcessIncomingMessage.hook(
 
         const command = message.Text.toUpperCase();
         if (command.startsWith("/BAITS")) {
-            const BaitsText = parseInt(command.split(" ")[1]);
+            let BaitsText = parseInt(command.split(" ")[1]);
             if (!isNaN(BaitsText)) {
-                quantityBait = BaitsText;
+                if (BaitsText >= 55) BaitsText = 55
+                
+                NewCombatText(
+                    Main.player[0].getRect(),
+                    Color.Blue,
+                    `Baits have been changed to ${BaitsText}`,
+                    true,
+                    false
+                ); 
+                quantityBait = BaitsText; 
                 saveConfig();
             }
         }
@@ -61,25 +76,24 @@ Player.ItemCheck_Shoot.hook((orig, self, i, sItem, weaponDamage) => {
     function MultiplyBaits(amount = 0) {
         activeBait = amount;
 
-        if (sItem.fishingPole > 0 && amount > 0) {
             Array.from(Main.projectile).forEach(projectile => {
                 if (amount <= 0) return;
-
-                Projectile['int NewProjectile(IEntitySource spawnSource, Vector2 position, Vector2 velocity, int Type, int Damage, float KnockBack, int Owner, float ai0, float ai1, float ai2)'](
-                    Projectile.GetNoneSource(),
-                    projectile.position,
-                    projectile.velocity,
-                    sItem.shoot,
-                    0,
-                    0,
-                    Main.myPlayer,
-                    0, 0, 0
-                );
-
-                amount--;
+                    if (sItem.fishingPole > 0 && amount > 0 && projectile.bobber) {
+                    Projectile['int NewProjectile(IEntitySource spawnSource, Vector2 position, Vector2 velocity, int Type, int Damage, float KnockBack, int Owner, float ai0, float ai1, float ai2)'](
+                        Projectile.GetNoneSource(),
+                        projectile.position,
+                        projectile.velocity,
+                        sItem.shoot,
+                        0,
+                        0,
+                        Main.myPlayer,
+                        0, 0, 0
+                    );
+    
+                    amount--;
+                }
             });
         }
-    }
 
     MultiplyBaits(quantityBait);
 });
